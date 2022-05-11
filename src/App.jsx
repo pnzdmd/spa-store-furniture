e372c98f86cb82ac10d1c068eea0e532ae35d99d
Oleg Novikov <pnzdmd@yandex.ru>
2022-05-11 13:52:05 +0300

    Добавить корзину

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,69 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Items from './components/Items/Items';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стул серый',
+          img: 'chair.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          category: 'chairs',
+          price: '45.99',
+        },
+        {
+          id: 2,
+          title: 'Стул белый',
+          img: 'chair2.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          category: 'chairs',
+          price: '45.99',
+        },
+        {
+          id: 3,
+          title: 'Стол',
+          img: 'table.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          category: 'chairs',
+          price: '45.99',
+        },
+        {
+          id: 4,
+          title: 'Стул серый',
+          img: 'chair.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          category: 'chairs',
+          price: '45.99',
+        },
+        {
+          id: 5,
+          title: 'Стул белый',
+          img: 'chair2.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          category: 'chairs',
+          price: '45.99',
+        },
+        {
+          id: 6,
+          title: 'Стол',
+          img: 'table.jpg',
+          desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          category: 'chairs',
+          price: '45.99',
+        },
+      ],
+    };
+  }
   render() {
     return (
       <div className='wrapper'>
         <Header />
+        <Items items={this.state.items} />
         <Footer />
       </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
 
 function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
+
   return (
     <header>
       <div>
@@ -10,6 +13,11 @@ function Header() {
           <li>Контакты</li>
           <li>Кабинет</li>
         </ul>
+        <FaShoppingCart
+          onClick={() => setCartOpen((cartOpen = !cartOpen))}
+          className={`shop-cart-button ${cartOpen && 'active'}`}
+        />
+        {cartOpen && <div className='shop-cart'></div>}
       </div>
       <div className='presentation'></div>
     </header>
